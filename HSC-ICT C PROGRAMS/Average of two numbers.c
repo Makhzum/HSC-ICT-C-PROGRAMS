dcb641a500fc408328655662d48472396d73c699
Makhzum Bin Harun <76556113+Makhzum@users.noreply.github.com>
2024-12-27 01:52:23 +0600

    Add files via upload

---
+++ HSC-ICT C PROGRAMS/Average of two numbers.c
@@ -0,0 +1,15 @@
+/*Average of two numbers*/
+
+#include <stdio.h>
+
+    main()
+{
+    float A, B, AVG;
+    printf("Enter the value of A:");
+    scanf("%f", &A);
+    printf("Enter the value of B:");
+    scanf("%f", &B);
+    AVG=(A+B)/2;
+    printf("Summation is %d", AVG);
+    return 0;
+}
---
+++ HSC-ICT C PROGRAMS/Summation of two numbers.c
@@ -0,0 +1,15 @@
+/*Summation of two numbers*/
+
+#include <stdio.h>
+
+    main()
+{
+    int A, B, SUM;
+    printf("Enter the value of A:");
+    scanf("%d", &A);
+    printf("Enter the value of B:");
+    scanf("%d", &B);
+    SUM=A+B;
+    printf("Summation is %d", SUM);
+    return 0;
+}
